51b3033246162edf0290b311a720c0d322d51232
Evgeni.ok14 <evgeni.ok@mail.ru>
2023-01-24 17:29:34 +0300

    wrote the work program

--- tusk/Program.cs
+++ tusk/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Clear();
+string s = Console.ReadLine();
+string [] arr = s.Split(" "); 
+string [] arr2 = new string[arr.Length];
+for(int i = 0; i < arr.Length; i++)
+{
+  if (arr[i].Length <= 3)
+  {
+    arr2[i] =arr[i];
+  }
+}
+    Console.WriteLine($"[{string.Join(" ", arr2)}]");
+  
+
+
+
